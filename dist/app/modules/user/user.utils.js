6cec93a031a9ef47c56ea19cd22a8c64b5660a7f
Baidyanath-k <baidyanathkumar12091998@gmail.com>
2024-06-08 10:19:48 +0600

    student ID

--- dist/app/modules/user/user.utils.js
+++ dist/app/modules/user/user.utils.js
@@ -14,11 +14,20 @@ const user_model_1 = require("./user.model");
 // Find Last Student ID
 const lastStudentId = () => __awaiter(void 0, void 0, void 0, function* () {
     const lastStudent = yield user_model_1.User.findOne({ role: 'student' }, { id: 1, _id: 0 }).sort({ createdAt: -1 }).lean();
-    return (lastStudent === null || lastStudent === void 0 ? void 0 : lastStudent.id) ? lastStudent.id.substring(6) : undefined;
+    return (lastStudent === null || lastStudent === void 0 ? void 0 : lastStudent.id) ? lastStudent.id : undefined;
 });
 // automatic student ID create (semester year, semester code, 4 digit code)
 const generateStudentId = (payload) => __awaiter(void 0, void 0, void 0, function* () {
-    const currentID = (yield lastStudentId()) || (0).toString();
+    let currentID = (0).toString();
+    // 2030 01 0000
+    const lastStudentIds = yield lastStudentId();
+    const lastSemesterYear = lastStudentIds === null || lastStudentIds === void 0 ? void 0 : lastStudentIds.substring(0, 4);
+    const lastSemesterCode = lastStudentIds === null || lastStudentIds === void 0 ? void 0 : lastStudentIds.substring(4, 6);
+    const currentSemesterYear = payload.year;
+    const currentSemesterCode = payload.code;
+    if (lastStudentIds && lastSemesterCode === currentSemesterCode && lastSemesterYear === currentSemesterYear) {
+        currentID = lastStudentIds.substring(6);
+    }
     let incrementID = (Number(currentID) + 1).toString().padStart(4, '0');
     incrementID = `${payload.year}${payload.code}${incrementID}`;
     return incrementID;

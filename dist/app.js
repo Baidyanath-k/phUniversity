4c3064cb8e2fb163109338bbbed08a88f1d99e37
Baidyanath-k <baidyanathkumar12091998@gmail.com>
2024-07-10 11:46:41 +0600

    token

--- dist/app.js
+++ dist/app.js
@@ -12,7 +12,7 @@ const notFoundRout_1 = require("./app/middlewares/notFoundRout");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({ origin: 'http://localhost:5173', credentials: true }));
 // route call by (allRoutes--> index.ts)
 app.use('/api/v1', allRoutes_1.default);
 app.get('/', (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 // route call by (allRoutes--> index.ts)
 app.use('/api/v1', router);
